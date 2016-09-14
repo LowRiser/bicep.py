de2cd6e96b45f9399811c94a4654c637f391555a
LowRiser <sensei940@yahoo.com>
2016-09-13 23:40:39 -0400

    added seth ai

---
+++ bicep.py
@@ -0,0 +1,19 @@
+print 'You\'re kidding me'
+import random
+
+def dumbbell():
+	x=random.randint(1,7)
+	if(x==1):
+		print'Quite frankly, you\'re kidding me'
+	elif(x==2):
+		print'Quite frankly, stay off the roof'
+	elif(x==3):
+		print'On my end, stay off the roof'
+	elif(x==4):
+		print'Go ahead and stay off the roof'
+	elif(x==5):
+		print'On my end, you\'re kidding me'
+	elif(x==6):
+		print'Quite frankly, I don\'t wear clothes'
+	else:
+		print'On my end, I dont\'t wear clothes'
